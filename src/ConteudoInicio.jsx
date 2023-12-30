0c958cf91019a960e8903d8469610f24cf9435d4
Lucas1nogueira <lucasnbbastos@outlook.com>
2023-12-30 15:16:31 -0300

    Atualizando info no projeto

--- src/ConteudoInicio.jsx
+++ src/ConteudoInicio.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import githubImg from "./github.png";
+import cImg from "./simbolo-c.png";
 import javaImg from "./simbolo-java.png";
 import pythonImg from "./simbolo-python.png";
 import jsImg from "./simbolo-js.jfif";
+import phpImg from "./simbolo-php.svg";
 
 function ConteudoInicio() {
   return (
@@ -14,10 +16,14 @@ function ConteudoInicio() {
       </p>
       <p>
         Meu nome é Lucas, sou aluno do curso de <b>Análise de Sistemas</b> no
-        IFBA e estou atualmente no 5º período.
+        IFBA, atualmente indo para o 6º período.
       </p>
-      <p>Sou iniciante nas seguintes linguagens:</p>
+      <p>Possuo conhecimento nas seguintes linguagens:</p>
       <ul>
+        <li>
+          <img src={cImg} alt="Símbolo C" />
+          <b>C</b>
+        </li>
         <li>
           <img src={javaImg} alt="Símbolo Java" />
           <b>Java</b>
@@ -30,17 +36,22 @@ function ConteudoInicio() {
           <img src={jsImg} alt="Símbolo JavaScript" />
           <b>JavaScript</b>
         </li>
+        <li>
+          <img src={phpImg} alt="Símbolo PHP" />
+          <b>PHP</b>
+        </li>
       </ul>
       <p>
-        Também desenvolvo em <b>HTML e CSS</b>, e comecei a ver a biblioteca{" "}
-        <b>React</b>, com a qual esse projeto foi feito. Comecei a aprender
-        também <b>Node.js</b> e <b>Flutter</b>.
+        Também desenvolvo em <b>HTML e CSS</b>, e desenvolvo também utilizando
+        a biblioteca <b>React</b> e o framework <b>Laravel</b>. Possuo
+        conhecimento também em <b>React Native</b> e <b>Flutter</b> para mobile,
+        bem como tenho noções básicas em <b>Typescript</b> e <b>Node.js</b>.
       </p>
       <p>Caso queira conhecer outros projetos, acesse a página abaixo:</p>
-      <div className="perfil">
+      <a href="https://github.com/Lucas1nogueira" className="perfil">
         <img src={githubImg} />
-        <a href="https://github.com/Lucas1nogueira">Perfil no GitHub</a>
-      </div>
+        <b style={{color: "#fff", marginLeft: 10}}>Perfil no GitHub</b>
+      </a>
     </div>
   );
 }
